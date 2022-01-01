d06b44547d2add05966f9c8df13db3f1ce87acea
bangkitdc@gmail.com <bangkitdc@gmail.com>
2022-01-02 01:59:21 +0700

    haha

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('client/switch.html'));
+app.use(express.static(__dirname));
 app.use('/css', express.static(__dirname + 'client/css'));
 app.use('/js', express.static(__dirname + 'client/js'));
 

2a41a17d210b83f94d4a895e94abb7c7126d3168
bangkitdc@gmail.com <bangkitdc@gmail.com>
2022-01-02 01:15:24 +0700

    yoman

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-// const PORT = 3000;
-// const express = require("express");
-// const app = express();
+const PORT = 3000;
+const express = require("express");
+const app = express();
 
-// const http = require('http');
-// const server = http.Server(app);
+const http = require('http');
+const server = http.Server(app);
 
-// app.use(express.static('client'));
+app.use(express.static('client'));
 
-// server.listen(PORT, function() {
-//     console.log('Server running');
-// })
+server.listen(PORT, function() {
+    console.log('Server running');
+})
 
-const app = require("express")();
+// const app = require("express")();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-  res.send("Hello world");
-});
+// app.get("", (req, res) => {
+//   res.send("Hello world");
+// });
 
-app.listen(PORT, () => {
-  console.log(`App up at port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App up at port ${PORT}`);
+// });

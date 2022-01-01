c0449d53c49229f772baf414f24a8cca6ecf9e1a
bangkitdc@gmail.com <bangkitdc@gmail.com>
2022-01-02 00:50:17 +0700

    test2

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const app = require("express")();
-
 const PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
 
-const http = requir('http');
+const http = require('http');
 const server = http.Server(app);
 
-app.use(express.static('client'));
+app.use(express.static('client/html'));
+
+server.listen(PORT, function() {
+    console.log('Server running');
+})
+

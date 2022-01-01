01e5cb6eef236433ad529f7ab4ab8d29247dacb0
bangkitdc@gmail.com <bangkitdc@gmail.com>
2022-01-02 02:02:39 +0700

    hese anyg

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static('client'));
 app.use('/css', express.static(__dirname + 'client/css'));
 app.use('/js', express.static(__dirname + 'client/js'));
 

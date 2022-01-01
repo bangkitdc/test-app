77af8914586cb8971815872c44c529424ad34a68
bangkitdc@gmail.com <bangkitdc@gmail.com>
2022-01-02 01:56:26 +0700

    p

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('client'));
+app.use(express.static('client/switch.html'));
 app.use('/css', express.static(__dirname + 'client/css'));
 app.use('/js', express.static(__dirname + 'client/js'));
-
-app.get('', function(req, res) {
-    res.sendFile(__dirname + '/switch.html');
-})
 
 app.listen(PORT, () => {
     console.log(`App up at port ${PORT}`)
